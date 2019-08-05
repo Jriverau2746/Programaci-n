44f67f8bcbc2d90ef74f5c515604169b193ef2d1
Jriverau2746 <53451590+Jriverau2746@users.noreply.github.com>
2019-08-05 17:46:45 -0500

    Add files via upload

---
+++ code 9.py
@@ -0,0 +1,43 @@
+'''Este mundo está diseñado para que el robot, Reeborg, 
+llegue al centro del mundo y coloque el objeto que lleva'''
+
+
+
+
+''' 
+Funcionalidad: LLegar al centro y colocar el objeto    
+pre: front is clear 
+pos: reeborg llegue a casa  
+'''
+
+def vuelta():
+    turn_left()
+    turn_left()
+ 
+
+def contar_pasos():
+    contador=1    
+    
+    while front_is_clear():
+        move()
+        contador = contador+1
+
+    vuelta()
+    mitad = int(contador/2)
+   
+    while mitad>0:
+        move()
+        mitad=mitad-1
+    
+    put()
+    
+contar_pasos()    
+
+while front_is_clear():
+    move()
+
+
+'''Autor: Juan F. Rivera'''
+'''Fecha: 05/07/2019'''
+
+    
