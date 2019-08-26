7618eb8a27e3ea6184a5e0d9b54c12ee11f0f24f
Jriverau2746 <53451590+Jriverau2746@users.noreply.github.com>
2019-08-26 18:01:28 -0500

    Add files via upload

---
+++ Actividad 5.py
@@ -0,0 +1,32 @@
+'''
+Descripción: Reeborg se mueve en cualquier dirección 
+(norte, nor-oeste, oeste, sur-oeste, sur, sur-este,
+este, nor-este) avanzando.
+'''
+'''
+Pre: Reeborg debe miarar el norte
+     Se debe expecificar la dirección siguiendo la 
+     siguiente instrucción:
+     
+    - Dirección:Indica hacia qué dirección se mueve 
+    Reeborg.
+    1<=dirección<=8
+    Siendo:
+    1=norte
+    2=nor-oeste
+    3=oeste
+    4=sur-oeste
+    5=sur
+    6=sur-este
+    7=este
+    8=nor-este
+    
+Pos:
+    True si el rey se puede mover a la dirección dada
+    False si el rey no se puede mover hasta la 
+    dirección dada y retorna a su posicción oroginal.
+'''
+
+
+
+
